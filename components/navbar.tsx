317bfe8e04be5bc798c26a6344602f7d88262432
andreamarangon <marangonandrea84@gmail.com>
2025-05-15 16:40:47 +0200

    Add Checkout and Products pages, update Navbar with links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,23 @@ import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link href="/">My Ecommerce</Link >
-      </div>
-      <div>
-        <Link href="/">Home</Link >
-        <Link href="/products">Products</Link >
-        <Link href="/checkout">Checkout</Link >
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <Link href="/" className="hover:text-blue-600">
+          My Ecommerce
+        </Link >
+        <ul className="hidden md:flex space-x-6">
+          <li>
+            <Link href="/">Home</Link >
+          </li>
+          <li className="hover:text-blue-600">
+            <Link href="/products">Products</Link >
+          </li>
+          <li className="hover:text-blue-600">
+            <Link href="/checkout">Checkout</Link >
+          </li>
+        </ul>
+        <div className="flex items-center space-x-4"></div>
       </div>
     </nav>
   )
